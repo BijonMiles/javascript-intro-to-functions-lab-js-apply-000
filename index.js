0888c251f0b2daab0c7687a061bfbf1be588a8d1
Bijon Miles <bijonmiles01@gmail.com>
2018-08-02 20:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
   else if (string.toUpperCase(string)){
-    return "YES INDEED"
+    return "YES INDEED! "
   }
+  else if (string.)
   
   return string.toUpperCase('HELLO')
   
