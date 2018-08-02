cc2bfd268a984a3c694c5228c9ff6a0599ce37e7
Bijon Miles <bijonmiles01@gmail.com>
2018-08-02 20:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string){
     return "I can't hear you!"
   }
-  else if (string.toUpperCase(string)){
+  else if (string.toUpperCase() === string){
     return "YES INDEED! "
   }
-  else if (string.)
+  else if (string === "I love you Grandma."){
+    return ""
+  }
   
   return string.toUpperCase('HELLO')
   
